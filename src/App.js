3f2248588ab4676c08672012231fe0f85feb1a23
sojjyCodes <ebenezeralagbe26@gmail.com>
2020-09-01 03:01:20 +0100

    Working on the Functional behaviours and responsiveness

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
-import React from 'react';
-import './App.css';
+import React, { Component, useState } from "react";
+import "./App.css";
+import {
+  Button,
+  Nav,
+  Navbar,
+  NavDropdown,
+  MenuItem,
+  NavItem,
+  Modal,
+} from "react-bootstrap";
+
+function Popup() {
+  alert("Are you sure you want to proceed");
+}
 
 function App() {
   return (
@@ -8,14 +21,46 @@ function App() {
         <h1 className="text">Login Page</h1>
       </div>
       <div className="box">
-        <div >
-        <center><input className type="text" name="findId" id = "IdInput" placeholder="Username or email" required autocapitalize="on" autofocus /></center>
-        <center><input className type="password" name="password" id = "passwordInput" placeholder="Password" required autocapitalize="on" autofocus /></center>
-        <center><input id = "loginBtn" type="submit" class="login-btn" value="Login" /></center>
+        <div>
+          <center>
+            <input
+              className
+              type="text"
+              name="findId"
+              id="IdInput"
+              placeholder="Username or email"
+              required
+              autocapitalize="on"
+              autofocus
+            />
+          </center>
+          <center>
+            <input
+              className
+              type="password"
+              name="password"
+              id="passwordInput"
+              placeholder="Password"
+              required
+              autocapitalize="on"
+              autofocus
+            />
+          </center>
+          <center>
+            <input
+              id="loginBtn"
+              type="submit"
+              class="login-btn"
+              value="Login"
+              onClick={() => Popup()}
+            />
+          </center>
         </div>
       </div>
-      <footer className="footer"> 
-      <p id = "footer-text"><center>Created with love by sojjyCodes ©2020</center></p>
+      <footer className="footer">
+        <p id="footer-text">
+          <center>Created with love by sojjyCodes ©2020</center>
+        </p>
       </footer>
     </div>
   );
